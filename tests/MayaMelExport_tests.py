fa73456a9205703fb854ba62c30d316065fcde1d
Daniel Forgacs <dforgacs@ad.blue2p0.com@bluewk010-l.ad.blue2p0.com>
2016-02-11 16:28:36 +0000

    started tests. simple comparison works

---
+++ tests/MayaMelExport_tests.py
@@ -0,0 +1,29 @@
+import os
+import unittest
+
+location = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+if location not in os.sys.path:
+    os.sys.path.append(location)
+
+
+try:
+    import MayaMelExport
+except:
+    gui = None
+
+
+class FileComparisonTest(unittest.TestCase):
+    def test__MayaMelExport__exports_same_mel(self):
+        with open('test_fixtures/JTJ_0010_v009.mel') as original:
+            original_export = original.read()
+
+        with open('test_fixtures/exports/JTJ_0010_v009.mel') as original:
+            new_export = original.read()
+
+        self.assertEqual(new_export, original_export)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
