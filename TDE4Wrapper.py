bcc27bf890af7e6d3e718193dc9c9a5331ae36da
forgacs.daniel <forgacs.daniel@gmail.com>
2016-02-21 03:16:37 +0000

    updated python API wrapper; added: focal, lens_id, filmback h/v

--- TDE4Wrapper.py
+++ TDE4Wrapper.py
@@ -14,22 +14,32 @@ class TDE4Wrapper(object):
             'res_x': ('getCameraImageWidth', 'cam_id'),
             'res_y': ('getCameraImageHeight', 'cam_id'),
             'path': ('getProjectPath',),
-            'footage': ('getCameraPath', 'cam_id')
+            'footage': ('getCameraPath', 'cam_id'),
+            'focal': ('getCameraFocalLength', 'cam_id', 'frame'),
+            'lens_id': ('getCameraLens', 'cam_id'),
+            'filmback_h': ('getLensFBackWidth', 'lens_id'),
+            'filmback_v': ('getLensFBackHeight', 'lens_id'),
         }
 
     def __getattr__(self, attr):
         if len(self.linker[attr]) == 1:
             return getattr(tde4, self.linker[attr][0])()
-        else:
-            return getattr(tde4, self.linker[attr][0])(self.cam_id)
+
+        elif len(self.linker[attr]) == 2:
+            parm = eval('self.' + self.linker[attr][1])
+            return getattr(tde4, self.linker[attr][0])(parm)
+
+        elif len(self.linker[attr]) == 3:
+            parm = eval('self.' + self.linker[attr][1])
+            return getattr(tde4, self.linker[attr][0])(parm, 1)
 
 
 
 
 def main():
     k = TDE4Wrapper()
-    print k.frange
-    print type(k.frange)
+    print (k.frange)
+    print (type(k.frange))
 
 
 if __name__ == '__main__':
